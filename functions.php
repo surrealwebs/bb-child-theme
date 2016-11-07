7f61bf9dc358a8dae984d4430cdf68fa9b2108f2
Adam Richards <arichard@nerdery.com>
2016-11-07 13:50:01 -0600

    added base beaver builder child theme

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+// Defines
+define( 'FL_CHILD_THEME_DIR', get_stylesheet_directory() );
+define( 'FL_CHILD_THEME_URL', get_stylesheet_directory_uri() );
+
+// Classes
+require_once 'classes/class-fl-child-theme.php';
+
+// Actions
+add_action( 'fl_head', 'FLChildTheme::stylesheet' );
